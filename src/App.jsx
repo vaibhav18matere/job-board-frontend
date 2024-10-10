d8267446121f31e739d92115c0598603b4c1b38f
VaibhavMatere <vpm00718897@gmail.com>
2024-10-10 12:51:42 +0530

    navbar refactored & react browser router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import { Navbar } from "./components/shared/Navbar";
-import { Footer } from "./components/shared/Footer";
+import { Login } from "./components/auth/Login";
+import { Signup } from "./components/auth/Signup";
+import Home from "./components/Home";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Footer />
-    </>
-  );
+  return <RouterProvider router={appRouter} />;
 }
 
 export default App;
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -7,31 +7,37 @@ import {
 import { Button } from "@/components/ui/button";
 import { FaRegUserCircle } from "react-icons/fa";
 import { AiOutlineLogout } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
-export const Navbar = () => {
+const Navbar = () => {
   const user = false;
 
   return (
     <div className="bg-white">
       <div className="flex items-center justify-between mx-auto max-w-7xl h-16 px-4">
         <div>
-          <h1 className="text-2xl font-bold">
-            Job<span className="text-[#F83002]">Board</span>
-          </h1>
+          <Link to="/">
+            <h1 className="text-2xl font-bold">
+              Job<span className="text-[#F83002]">Board</span>
+            </h1>
+          </Link>
         </div>
         <div className="flex items-center gap-12">
           <ul className="flex font-medium items-center gap-5">
-            <li>Home</li>
             <li>Jobs</li>
             <li>Browse</li>
           </ul>
 
           {!user ? (
             <div className="flex items-center gap-2">
-              <Button variant="outline">Login</Button>
-              <Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">
-                Signup
-              </Button>
+              <Link to="/login">
+                <Button variant="outline">Login</Button>
+              </Link>
+              <Link to="/signup">
+                <Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">
+                  Signup
+                </Button>
+              </Link>
             </div>
           ) : (
             <Popover>
@@ -77,3 +83,5 @@ export const Navbar = () => {
     </div>
   );
 };
+
+export default Navbar;
