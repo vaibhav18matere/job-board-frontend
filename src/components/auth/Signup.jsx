d4a174c759c344e914564f8f5bce0f951c625305
VaibhavMatere <vpm00718897@gmail.com>
2024-10-10 13:15:48 +0530

    signup form created; data & wiring checked in console

---
+++ src/components/auth/Signup.jsx
@@ -0,0 +1,128 @@
+import { useState } from "react";
+import Navbar from "../shared/Navbar";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { RadioGroup } from "../ui/radio-group";
+import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
+
+export const Signup = () => {
+  const [input, setInput] = useState({
+    fullname: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    role: "",
+    file: "",
+  });
+
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const signupFormHandler = async (e) => {
+    e.preventDefault();
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div className="flex items-center justify-center max-w-7xl mx-auto">
+        <form
+          onSubmit={signupFormHandler}
+          className="w-1/2 border border-gray-200 rounded-md p-4 my-10"
+        >
+          <h1 className="font-bold text-xl mb-5">Signup</h1>
+          <div className="my-2">
+            <Label>Full Name</Label>
+            <Input
+              type="text"
+              value={input.fullname}
+              name="fullname"
+              onChange={changeEventHandler}
+              placeholder="johnrao devkar"
+              className="mt-2"
+            />
+          </div>
+          <div className="my-2">
+            <Label>Email</Label>
+            <Input
+              type="email"
+              value={input.email}
+              name="email"
+              onChange={changeEventHandler}
+              placeholder="abc@gmail.com"
+              className="mt-2"
+            />
+          </div>
+          <div className="my-2">
+            <Label>Phone Number</Label>
+            <Input
+              type="text"
+              value={input.phoneNumber}
+              name="phoneNumber"
+              onChange={changeEventHandler}
+              placeholder="9876543210"
+              className="mt-2"
+            />
+          </div>
+          <div className="my-2">
+            <Label>Password</Label>
+            <Input
+              type="password"
+              value={input.password}
+              name="password"
+              onChange={changeEventHandler}
+              placeholder="xxxxxxxxx"
+              className="mt-2"
+            />
+          </div>
+          <div className="flex items-center justify-start">
+            <RadioGroup className="flex items-center gap-5 my-5">
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="student"
+                  checked={input.role === "student"}
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="r1">Student</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="recruiter"
+                  checked={input.role === "recruiter"}
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="r2">Recruiter</Label>
+              </div>
+            </RadioGroup>
+          </div>
+          <div className="flex items-center gap-2">
+            <Input accept="image/*" type="file" className="cursor-pointer" />
+          </div>
+
+          <Button
+            className="bg-[#6A38C2] hover:bg-[#5b30a6] w-full my-4"
+            type="submit"
+          >
+            Signup
+          </Button>
+
+          <div className="text-sm">
+            Already have an account?
+            <Link to="/login" className="text-blue-600">
+              Login
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
